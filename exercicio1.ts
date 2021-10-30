0d60aba03c48b1ca9bc4c804da1bc29baa40da86
Letícia Genes <leticia_genes@hotmail.com>
2021-10-30 03:33:28 -0400

    comentado

--- exercicio1.ts
+++ exercicio1.ts
@@ -1,23 +1,27 @@
+//função chamada ao clicar no botão
+function mostrarResultado() {
+    //variável que recebe a palavra escrita via input
+    let palavra: string = (<HTMLSelectElement>document.getElementById('palavra')).value;
+    //variável que recebe o retorno da função com a quantidade de vogais
+    let quant: number = contarVogais(palavra);
+    //cria um parágrafo para mostrar a resposta
+    document.body.innerHTML = `<p>Quantidade de vogais da palavra <strong>${palavra}</strong>: ${quant}</p>`;
+}
 
-//function contarVogaisParametro()
-//function contarVogais() : Number {
-function contarVogais(palavra: string = (<HTMLSelectElement>document.getElementById('palavra')).value) {
-    let quant: number = 0;
+//função que conta o número de vogais da palavra
+function contarVogais(palavra: string) : number {
+    let cont: number = 0;                   //contador
 
-    for(let i=0; i<palavra.length; i++) {
-        switch (palavra[i]) {
+    for(let i=0; i<palavra.length; i++) {   //percorre todas as letras da palavra
+        switch (palavra[i]) {               //verifica se a letra corrente corresponde a uma vogal
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
-                quant++;
-                break;
+                cont++;                     //em caso positivo, incrementa o contador
+                break;                      //sai do laço switch
         }
     }
-    document.body.innerHTML = `<p>A palavra ${palavra} possui ${quant} vogais</p>`
-    console.log(quant);
-    //return quant;
+    return cont;                            //retorna o número total de vogais encontradas
 }
-
-//console.log(quantasVogais())
--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,22 +1,26 @@
-//function contarVogaisParametro()
-//function contarVogais() : Number {
+//função chamada ao clicar no botão
+function mostrarResultado() {
+    //variável que recebe a palavra escrita via input
+    var palavra = document.getElementById('palavra').value;
+    //variável que recebe o retorno da função com a quantidade de vogais
+    var quant = contarVogais(palavra);
+    //cria um parágrafo para mostrar a resposta
+    document.body.innerHTML = "<p>Quantidade de vogais da palavra <strong>" + palavra + "</strong>: " + quant + "</p>";
+}
+//função que conta o número de vogais da palavra
 function contarVogais(palavra) {
-    if (palavra === void 0) { palavra = document.getElementById('palavra').value; }
-    var quant = 0;
-    for (var i = 0; i < palavra.length; i++) {
-        switch (palavra[i]) {
+    var cont = 0; //contador
+    for (var i = 0; i < palavra.length; i++) { //percorre todas as letras da palavra
+        switch (palavra[i]) { //verifica se a letra corrente corresponde a uma vogal
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
-                quant++;
-                break;
+                cont++; //em caso positivo, incrementa o contador
+                break; //sai do laço switch
         }
     }
-    document.body.innerHTML = "<p>A palavra " + palavra + " possui " + quant + " vogais</p>";
-    console.log(quant);
-    //return quant;
+    return cont; //retorna o número total de vogais encontradas
 }
-//console.log(quantasVogais())
 //# sourceMappingURL=exercicio1.js.map
